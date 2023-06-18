eb95f090c9cf4b721c320dc5e2948011afeff985
Ivy Tan <tan_ivy31@gmail.com>
2023-06-18 21:24:43 +0800

    Tried styled components

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+} from './NavbarElements';
+
+const Navbar = ({ toggle }) => {
+    return (
+        // these are fragments: <>
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/">dolla</NavLogo>{' '}
+                    {/* the to here is href in a */}
+                    <MobileIcon onClick={toggle}>
+                        <FaBars /> {/* from react icons */}
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="discover">Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="services">Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="signup">Sign Up</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
