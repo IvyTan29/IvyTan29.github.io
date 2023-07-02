217676b6cadc766c6bb1832ad772c7e6e196a9fb
Ivy Tan <tan_ivy31@gmail.com>
2023-07-02 19:01:15 +0800

    set up react and added component

---
+++ tutorial/src/components/Hello.js
@@ -0,0 +1,9 @@
+// functional compoment
+/* function Hello(){
+    return <h1>Hello World!</h1>
+} */
+
+// ES6 function
+const Hello = () => <h1>Hello There</h1>
+
+export default Hello; //default-> import in any file and give it any sort of name
