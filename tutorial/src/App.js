0c3eb140575c84c5f8c507964ea750dc107006fc
Ivy Tan <tan_ivy31@gmail.com>
2023-07-02 21:19:29 +0800

    props in functional component

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Profile from './components/Profile';
 
 function App() {
     return (
         <div className="App">
-            <Hello />
-            <Message />
+            <Profile name="Ivy" lastname="Tan">
+              <h3>This is a profile of a person</h3>
+            </Profile>
+            <Profile name="Catherine" lastname="Jones" />
         </div>
     );
 }
---
+++ tutorial/src/components/Profile.js
@@ -0,0 +1,10 @@
+const Profile = (props) => {
+    return (
+        <h1>
+            Name: {props.name} {props.lastname}
+            {props.children}
+        </h1>
+    );
+};
+
+export default Profile;
