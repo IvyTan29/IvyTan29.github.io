25b5a53279f1b116d328fdd33a04c78aedd2c5ce
Ivy Tan <tan_ivy31@gmail.com>
2023-07-02 19:34:15 +0800

    JSX

--- tutorial/src/components/Hello.js
+++ tutorial/src/components/Hello.js
@@ -1,9 +1,18 @@
+const name = 'Ivy';
+
+const displayMessage = () => {
+    return 'I need help!';
+};
 // functional compoment
 /* function Hello(){
     return <h1>Hello World!</h1>
 } */
 
 // ES6 function
-const Hello = () => <h1>Hello There</h1>
+const Hello = () => (
+    <h1>
+        Total is {10 + 20}, {displayMessage()}
+    </h1>
+);
 
 export default Hello; //default-> import in any file and give it any sort of name
