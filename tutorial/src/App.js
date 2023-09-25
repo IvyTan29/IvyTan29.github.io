267756ae21cd6b8dabdc082afe60450eee10f6f1
Ivy Tan <tan_ivy31@gmail.com>
2023-09-25 20:51:35 +0800

    Event Handling in Class Based Components

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -6,6 +6,7 @@ import Profile from './components/Profile';
 import Counter from './components/Counter';
 import Resume from './components/Resume';
 import FunctionEvent from './components/FunctionEvent';
+import ClassEvent from './components/ClassEvent';
 
 function App() {
     return (
@@ -13,9 +14,8 @@ function App() {
             {/* <Profile name="Ivy" lastname="Tan" /> */}
             {/* <Counter></Counter> */}
             {/* <Resume name="Ivy"></Resume> */}
-            <FunctionEvent>
-                
-            </FunctionEvent>
+            {/* <FunctionEvent></FunctionEvent> */}
+            <ClassEvent></ClassEvent>
         </div>
     );
 }
