dcb05f283a4fb19c73148a03d24cf393839933e2
Ivy Tan <tan_ivy31@gmail.com>
2023-10-02 21:02:51 +0800

    States in Functional Components

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -4,6 +4,7 @@ import Hello from './components/Hello';
 import Message from './components/Message';
 import Profile from './components/Profile';
 import Counter from './components/Counter';
+import FunctionalCounter from './components/FunctionalCounter';
 import Resume from './components/Resume';
 import FunctionEvent from './components/FunctionEvent';
 import ClassEvent from './components/ClassEvent';
@@ -13,9 +14,10 @@ function App() {
         <div className="App">
             {/* <Profile name="Ivy" lastname="Tan" /> */}
             {/* <Counter></Counter> */}
+            <FunctionalCounter></FunctionalCounter>
             {/* <Resume name="Ivy"></Resume> */}
             {/* <FunctionEvent></FunctionEvent> */}
-            <ClassEvent></ClassEvent>
+            {/* <ClassEvent></ClassEvent> */}
         </div>
     );
 }
